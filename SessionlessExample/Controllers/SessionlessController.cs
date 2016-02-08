681e0402f3fc56bfec3478c005d34cb42bd74bdc
Vinicius Mussak <vinicius.mussak@outlook.com>
2016-02-08 17:47:11 -0200

    Removendo cache annotation

--- SessionlessExample/Controllers/SessionlessController.cs
+++ SessionlessExample/Controllers/SessionlessController.cs
@@ -3,7 +3,6 @@
 namespace SessionlessExample.Controllers
 {
     [SessionState(System.Web.SessionState.SessionStateBehavior.ReadOnly)]
-    [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
     public class SessionlessController : Controller
     {
         public ActionResult Index(int index)
